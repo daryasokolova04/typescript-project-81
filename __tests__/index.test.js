216bc154ccb4010ebdfd0b23512412ac3bfc634c
Daria Sokolova <darya@Darias-MacBook-Air.local>
2024-07-26 21:42:21 +0200

    expect fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,15 +3,15 @@ import Tag from "../src/index";
 
 test("<br>", () => {
   const tag = new Tag("br").toString();
-  expect(tag.toEqual("<br>"));
+  expect(tag).toEqual("<br>");
 });
 
 test('<img src="path/to/image">', () => {
   const tag = new Tag("img", { src: "path/to/image" }).toString();
-  expect(tag.toEqual('<img src="path/to/image">'));
+  expect(tag).toEqual('<img src="path/to/image">');
 });
 
 test('<input type="submit" value="Save">', () => {
   const tag = new Tag("input", { type: "submit", value: "Save" }).toString();
-  expect(tag.toEqual('<input type="submit" value="Save">'));
+  expect(tag).toEqual('<input type="submit" value="Save">');
 });
